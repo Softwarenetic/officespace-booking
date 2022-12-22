fbe3a329d446fc4d3f0a46f5768e630683cd945c
“AndriiZaritskyi” <andrii.zaritskyi@softwarenetic.com>
2022-12-22 16:48:28 +0200

    added vertical alignment

--- packages/frontend_user/src/components/SignInWindow/SignInWindow.tsx
+++ packages/frontend_user/src/components/SignInWindow/SignInWindow.tsx
@@ -2,15 +2,26 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import './SignInWindow.css'
+import Grid from '@mui/material/Grid';
 
 
 const SignInWindow:React.FC = ()=>{
 
     return (
-        <Box className='signInContainer' >
-            <Typography  variant="h4" gutterBottom> Workplace Booking System </Typography>
-            <Button variant="contained" size="small" style={{margin:'20px',padding:'5px 40px',backgroundColor:'black'}}>Log in with Google </Button>
-        </Box>
+       <Grid
+       container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+       >
+            <Box className='signInContainer' >
+                <Typography  variant="h4"> Workplace Booking System </Typography>
+                <Button variant="contained" size="small" style={{margin:'20px',padding:'8px 45px',backgroundColor:'black'}}>Log in with Google </Button>
+            </Box>
+       </Grid>
+       
     )
 
 }
