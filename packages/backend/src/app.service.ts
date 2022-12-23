2c701447d4dcc8e6b892eb1fdc3a24af56697d09
junkiezet <junkiezet@gmail.com>
2022-12-23 14:18:24 +0200

    Backend authentification introdused; Add user data into database

--- packages/backend/src/app.service.ts
+++ packages/backend/src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {User} from './entity/User';
+import {AppDataSource} from "./data-source";
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async login({
+                email,
+                name,
+                image,
+              }: {
+    email: string;
+    name: string;
+    image: string;
+  }): Promise<any> {
+    const user = await AppDataSource.manager.findOneBy(User, {email: email});
+    if (!user) {
+      const newUser = new User();
+      newUser.email = email;
+      newUser.name = name;
+      newUser.avatar = image;
+      await AppDataSource.manager.save(newUser);
+      return newUser;
+    } else {
+      console.log(user);
+      return user;
+    }
   }
 }
