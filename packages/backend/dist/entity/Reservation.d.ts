7a3ce67e1ee7f499394fedc94fd654fb8416fb77
Bohdan Tkachyk <bohdan.tkachyk@softwarenetic.com>
2022-12-23 16:23:00 +0200

    removed gitignore files

--- packages/backend/dist/entity/Reservation.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Reservation {
-    id: number;
-    from: string;
-    to: string;
-    user: number;
-    workplace: number;
-    office: number;
-}
-export default Reservation;
--- packages/backend/dist/migration/1671694571441-CreateOffice.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class CreateOffice1671694571441 implements MigrationInterface {
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
