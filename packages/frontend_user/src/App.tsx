769ab4a984b5870ab11915fa588a5fb02a7ce610
junkiezet <junkiezet@gmail.com>
2022-12-23 14:24:42 +0200

    Frontend authentification introdused;

--- packages/frontend_user/src/App.tsx
+++ packages/frontend_user/src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {GoogleOAuthProvider, GoogleLogin} from "@react-oauth/google";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+    const HOST = process.env.HOST;
+
+    const onFailure = (): void => {
+        console.log('failed');
+    };
+
+    return (
+        <div className="App">
+            <main>
+                <div>
+                    <h2>React Google Login</h2>
+                    <br/>
+                    <GoogleOAuthProvider
+                        clientId={GOOGLE_CLIENT_ID!}
+                    >
+                        <GoogleLogin
+                            onSuccess={async (credentialResponse) => {
+                                console.log(credentialResponse);
+                                const {data} = await axios.post(
+                                    `${HOST}/login`,
+                                    {
+                                        token: credentialResponse.credential,
+                                    }
+                                );
+                                localStorage.setItem("AuthData", JSON.stringify(data));
+                                alert('success');
+                            }}
+                            onError={onFailure}
+                        />
+                    </GoogleOAuthProvider>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
