43913ffcfaa1850e9bf45a6c8a5a2070e9a09656
“AndriiZaritskyi” <andrii.zaritskyi@softwarenetic.com>
2022-12-23 15:34:47 +0200

    fixing...

--- packages/frontend_user/src/store/reducers/ActionCreators.ts
+++ packages/frontend_user/src/store/reducers/ActionCreators.ts
@@ -2,8 +2,8 @@ import { IUser } from "../../models/IUser";
 import { AppDispatch } from "../store";
 
 
-export const fetchUsers = ()=>async (dispatch:AppDispatch)=>{
-    try{
-        const response = await fetch<IUser[]>('')
-    }
-}
+     //export const fetchUsers = ()=>async (dispatch:AppDispatch)=>{
+     //  try{
+     //     const response = await fetch<IUser[]>('')
+     // }
+     //} 
