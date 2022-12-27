0264a75cb5f12d92001192b4c4b61321110f18b6
Bohdan Tkachyk <bohdan.tkachyk@softwarenetic.com>
2022-12-27 14:40:11 +0200

    configs in separate folder; sign in

--- packages/frontend_user/src/pages/sign-in/SignInWindow.tsx
+++ packages/frontend_user/src/pages/sign-in/SignInWindow.tsx
@@ -1,20 +1,15 @@
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import React, { useEffect } from "react";
 import { gapi } from "gapi-script";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import "./SignInWindow.scss";
+import {configs} from '../../config/config';
+
 
 const SignInWindow: React.FC = () => {
- 
-
-  const GOOGLE_CLIENT_ID =
-    "932663426648-82mkdignd0jnvhgpbvv8mdiloo40h6rr.apps.googleusercontent.com";
-  alert(GOOGLE_CLIENT_ID);
   useEffect(() => {
     const initClient = () => {
       gapi.client.init({
@@ -40,25 +35,13 @@ const SignInWindow: React.FC = () => {
     >
       <Box className="signInContainer">
         <Typography variant="h4"> Workplace Booking System </Typography>
-        <Button
-          variant="contained"
-          size="small"
-          style={{
-            margin: "20px",
-            padding: "8px 45px",
-            backgroundColor: "black",
-          }}
-          href="http://localhost:4000/login"
-        >
-          Log in with Google{" "}
-        </Button>
       </Box>
-      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID!}>
+      <GoogleOAuthProvider clientId={configs.googleClientId}>
         <GoogleLogin
           onSuccess={async (credentialResponse) => {
             console.log(credentialResponse);
             const { data } = await axios.post(
-              "http://localhost:4000/google/redirect",
+              `${configs.baseUrl}/login`,
               {
                 token: credentialResponse.credential,
               }
