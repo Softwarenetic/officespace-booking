0ab3834eacafe3b166ddcfd73aa9a8b23b395049
junkiezet <junkiezet@gmail.com>
2022-12-28 17:21:43 +0200

    Fix secure in update route;

--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '../entity/User';
+import {Injectable} from '@nestjs/common';
+import {User} from '../entity/User';
 import {AppDataSource} from "../config/typeorm.config";
 
 @Injectable()
 export class UserService {
 
-  async update(id: number, user: User) {
-    await AppDataSource.manager.update(User, user.id, user);
-    return `Successfully updated a #${user.id} user`;
-  }
+    async update(id: number, user: User) {
+        delete user.id;
+        delete user.email;
+        const existing = await AppDataSource.manager.findOne(User, {
+            where: {id}
+        });
 
-  async remove(id: number) {
-    await AppDataSource.manager.delete(User, {id:id});
-    return `Successfully removed a #${id} user`;
-  }
+        const updated = await AppDataSource.manager.save(User, {
+            ...existing,
+            ...user
+        });
+        console.log(updated);
+
+        return `Successfully updated a #${id} user`;
+    }
+
+    async remove(id: number) {
+        await AppDataSource.manager.delete(User, {id: id});
+        return `Successfully removed a #${id} user`;
+    }
 }
