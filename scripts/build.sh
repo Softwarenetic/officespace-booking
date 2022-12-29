43c6bb44e53e12b5b16b44759e1527efcd420151
Volodymyr Lysetskyi <volodymyr.lysetskyi@softwarenetic.com>
2022-12-29 17:54:45 +0200

    fixed script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,4 @@
-cd ../packages
-# cd frontend_admin
-# npm i
-# yarn build
-# sudo cp -r ./build /var/www/html/admin/build
-# echo "admin built"
-# cd ../frontend_user
-# npm i
-# yarn build
-# sudo cp -r ./build /var/www/html/user/build
-# echo "user built"
-cd ../backend
+cd ../packages/backend
 npm i
 pm2 --name backend start npm -- start
 echo "backend started"
