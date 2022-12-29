59d6b7370b85000cb0e0867eb59d8dea4d428147
Volodymyr Lysetskyi <volodymyr.lysetskyi@softwarenetic.com>
2022-12-29 14:44:33 +0200

    buildinf files inside pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 cd ../packages
-cd frontend_admin
-npm i
-yarn build
-sudo cp -r ./build /var/www/html/admin/build
-echo "admin built"
-cd ../frontend_user
-npm i
-yarn build
-sudo cp -r ./build /var/www/html/user/build
-echo "user built"
+# cd frontend_admin
+# npm i
+# yarn build
+# sudo cp -r ./build /var/www/html/admin/build
+# echo "admin built"
+# cd ../frontend_user
+# npm i
+# yarn build
+# sudo cp -r ./build /var/www/html/user/build
+# echo "user built"
 cd ../backend
 npm i
 pm2 --name backend start npm -- start
