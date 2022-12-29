5e600f3444f14d1e932ac5ed7791760ac044bc94
AndriiZaritskyi <121103436+AndriiZaritskyi@users.noreply.github.com>
2022-12-29 10:03:43 +0200

    Created profile user markup (#22)
    Co-authored-by: “AndriiZaritskyi” <“andrii.zaritskyi@softwarenetic.com”>

--- packages/frontend_user/src/routing/routing.tsx
+++ packages/frontend_user/src/routing/routing.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/mainPage/MainPage";
 import RootLayout from "../pages/rootLayout/RootLayout";
 import SignInWindow from "../pages/sign-in/SignInWindow";
+import UserProfile from "../pages/userProfile/UserProfile";
 import UserSettings from "../pages/userSettings/UserSettings";
 
 export const routing = createBrowserRouter([
@@ -15,8 +16,9 @@ export const routing = createBrowserRouter([
       },
       {
         path: "/settings",
-        element: <UserSettings />,
+        element: <UserProfile />,
       },
+      
     ],
   },
   {
