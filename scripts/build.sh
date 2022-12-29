b562c3a1316f832c90b114fa391cb93c5d27d6c9
Volodymyr Lysetskyi <volodymyr.lysetskyi@softwarenetic.com>
2022-12-29 13:53:49 +0200

    added echo

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,15 @@ cd frontend_admin
 npm i
 yarn build
 sudo cp -r ./build /var/www/html/admin/build
+echo "admin built"
 cd ../frontend_user
 npm i
 yarn build
 sudo cp -r ./build /var/www/html/user/build
+echo "user built"
 cd ../backend
 npm i
 pm2 --name backend start npm -- start
+echo "backend started"
 sudo nginx -s reload
+echo "nginx restarted"
