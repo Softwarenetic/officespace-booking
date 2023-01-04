ac0620a31eb6135072408e431d3bbb1bab10935d
user <ihor.tsymbaliuk@softwarenetic.com>
2023-01-04 10:39:50 +0200

    Uploading image (frontend);

--- packages/frontend_user/src/pages/sign-in/SignInWindow.tsx
+++ packages/frontend_user/src/pages/sign-in/SignInWindow.tsx
@@ -1,68 +1,68 @@
 import Box from '@mui/material/Box';
-import React, { useEffect } from 'react';
-import { gapi } from 'gapi-script';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import React, {useEffect} from 'react';
+import {gapi} from 'gapi-script';
+import {GoogleOAuthProvider, GoogleLogin} from '@react-oauth/google';
 import axios from 'axios';
 import './SignInWindow.scss';
-import { configs } from '../../config/config';
-import { useAppDispatch } from '../../hooks/redux';
-import { userSlice } from '../../store/reducers/UserSlice';
-import { useNavigate } from 'react-router-dom';
-import { loginSuccess } from '../../store/reducers/UserSlice';
+import {configs} from '../../config/config';
+import {useAppDispatch} from '../../hooks/redux';
+import {userSlice} from '../../store/reducers/UserSlice';
+import {useNavigate} from 'react-router-dom';
+import {loginSuccess} from '../../store/reducers/UserSlice';
 import picture from '../../assets/61f134d31a80c10b8052cbb30c7a46ae.jpg';
 
 const SignInWindow: React.FC = () => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        scope: ['profile', 'email'],
-      });
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: process.env.GOOGLE_CLIENT_ID,
+                scope: ['profile', 'email'],
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
+
+    const onFailure = () => {
+        console.log('failed');
     };
-    gapi.load('client:auth2', initClient);
-  });
 
-  const onFailure = () => {
-    console.log('failed');
-  };
+    dispatch(userSlice.actions.loginSuccess);
 
-  dispatch(userSlice.actions.loginSuccess);
-
-  return (
-    <Box className="main_container">
-        <Box className='polygon_1 triangle_3'></Box>
-        <Box className="triangle_1"></Box>
-        <Box className="triangle_2"></Box>
-        <Box className='triangle_3'></Box>
-      <Box className='signPicture'>
-        <img src={picture} alt="picture" />
-     </Box>
-      <Box className="sign_container">
-        <Box className='logo_container'>
-          <p className='title'> Workplace Booking System </p>
-          <Box className='sign_btn'>
-          <GoogleOAuthProvider clientId={configs.googleClientId}  >
-            <GoogleLogin
-              onSuccess={async (credentialResponse) => {
-                console.log(credentialResponse);
-                const { data } = await axios.post(`${configs.baseUrl}/login`, {
-                  token: credentialResponse.credential,
-                });
-                localStorage.setItem('AuthData', JSON.stringify(data.data));
-                dispatch(loginSuccess());
-                return data.message === 'success' ? navigate('/') : alert(data.message);
-              }}
-              onError={onFailure}
-             />
-          </GoogleOAuthProvider>
-          </Box>
+    return (
+        <Box className="main_container">
+            <Box className='polygon_1 triangle_3'></Box>
+            <Box className="triangle_1"></Box>
+            <Box className="triangle_2"></Box>
+            <Box className='triangle_3'></Box>
+            <Box className='signPicture'>
+                <img src={picture} alt="background_picture"/>
+            </Box>
+            <Box className="sign_container">
+                <Box className='logo_container'>
+                    <p className='title'> Workplace Booking System </p>
+                    <Box className='sign_btn'>
+                        <GoogleOAuthProvider clientId={configs.googleClientId}>
+                            <GoogleLogin
+                                onSuccess={async (credentialResponse) => {
+                                    console.log(credentialResponse);
+                                    const {data} = await axios.post(`${configs.baseUrl}/login`, {
+                                        token: credentialResponse.credential,
+                                    });
+                                    localStorage.setItem('AuthData', JSON.stringify(data.data));
+                                    dispatch(loginSuccess());
+                                    return data.message === 'success' ? navigate('/') : alert(data.message);
+                                }}
+                                onError={onFailure}
+                            />
+                        </GoogleOAuthProvider>
+                    </Box>
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default SignInWindow;
